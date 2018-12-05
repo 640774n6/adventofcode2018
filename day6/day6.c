4c16bb08b25aa0646a2dcc6f3b16fdb098a9ca6a
Brandon Holland <brandon.holland@catalystrms.com>
2018-12-05 15:14:05 -0800

    Added xcodeproj ignore and day6 prep

---
+++ day6/day6.c
@@ -0,0 +1,32 @@
+//
+//  day6.c
+//  day6
+//
+//  Created by Brandon Holland on 2018-12-03.
+//  Copyright © 2018 Brandon Holland. All rights reserved.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+
+#define MAX(x, y) (((x) > (y)) ? (x) : (y))
+#define MIN(x, y) (((x) < (y)) ? (x) : (y))
+
+int main(int argc, char *argv[])
+{
+    //Argument check
+    if (argc < 2)
+    {
+        printf("usage: day6 <INPUT>\n");
+        exit(0);
+    }
+    
+    //Load input
+    FILE *file = fopen(argv[1], "r");
+    fclose(file);
+
+    //Cleanup
+    return 0;
+}
