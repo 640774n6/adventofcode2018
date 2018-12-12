c4640dc6fb737a1014da0dfb36dda2823160929f
Brandon Holland <brandon.holland@catalystrms.com>
2018-12-12 15:23:05 -0800

    Optimization/Flexibility cleanup pass on day 12

--- day12/day12.c
+++ day12/day12.c
@@ -14,8 +14,6 @@
 #define MAX(x, y) (((x) > (y)) ? (x) : (y))
 #define MIN(x, y) (((x) < (y)) ? (x) : (y))
 
-#define STATE_PADDING   300
-
 unsigned int calculateRuleIndex(char *rule)
 {
     unsigned int ruleIndex = 0;
@@ -30,23 +28,32 @@ unsigned int calculateRuleIndex(char *rule)
 
 int64_t calculateStateSum(char *ruleResultMap,
                           char *initialState,
-                          size_t stateCount,
-                          int zeroIndex,
+                          int initialStateCount,
                           int64_t generationCount)
 {
-    //Initialize states
-    char *newState = malloc(sizeof(char) * stateCount);
-    char *currentState = malloc(sizeof(char) * stateCount);
-    memcpy(currentState, initialState, sizeof(char) * stateCount);
-    
-    //Start generation loop
+    //Initialize variables
+    int firstPotId = -4;
+    int workingStateCount = initialStateCount + 8;
     int64_t lastStateSum = 0;
     int64_t lastDelta = 0;
+    
+    //Initialize current state
+    char *currentState = malloc(sizeof(char) * workingStateCount);
+    memset(currentState, '.', sizeof(char) * workingStateCount);
+    memcpy(currentState + 4, initialState, sizeof(char) * initialStateCount);
+    
+    //Initialize new state
+    char *newState = malloc(sizeof(char) * workingStateCount);
+    memset(newState, '.', sizeof(char) * workingStateCount);
+    
+    //Start generations
     for(int64_t g = 1; g < generationCount + 1; g++)
     {
         //Get next generation
-        int64_t currentStateSum = 0;
-        for(int i = 2; i < stateCount - 2; i++)
+        int64_t newStateSum = 0;
+        int firstPlant = 0;
+        int newStateIndex = 4;
+        for(int i = 2; i < workingStateCount - 2; i++)
         {
             //Get rule for current index
             //Calculate rule index using rule
@@ -54,29 +61,61 @@ int64_t calculateStateSum(char *ruleResultMap,
             char *rule = currentState + i - 2;
             int ruleIndex = calculateRuleIndex(rule);
             char ruleResult = ruleResultMap[ruleIndex];
+
+            //If pot results in a plant
+            if(ruleResult == '#')
+            {
+                //If this is the first plant for this generation
+                if(!firstPlant)
+                {
+                    //Toggle first plant and reset first pot id
+                    firstPlant = 1;
+                    firstPotId += i - newStateIndex;
+                }
+                
+                //Update new state sum and new state
+                int newPlantId = (firstPotId + newStateIndex);
+                newStateSum += newPlantId;
+                newState[newStateIndex] = ruleResult;
+            }
             
-            //Set result to index in new state and increment current state sum
-            newState[i] = ruleResult;
-            currentStateSum += (ruleResult == '#') ? (i - zeroIndex) : 0;
+            //Increment new state index only if we've already found the first plant
+            newStateIndex += firstPlant ? 1 : 0;
         }
         
-        //If last state sum delta is the same, we have stabalized
-        int64_t d = currentStateSum - lastStateSum;
+        //If last state sum delta is the same, we have stabilized
+        int64_t d = newStateSum - lastStateSum;
         if(lastDelta == d)
         {
-            //Use stabalized delta to calculate final state sum
+            //Use stablized delta to calculate final state sum
             int64_t gDelta = generationCount - g;
-            lastStateSum = currentStateSum + (d * gDelta);
+            lastStateSum = newStateSum + (d * gDelta);
             break;
         }
         
-        //Swap current and new
-        char *tmp = currentState;
-        currentState = newState;
-        newState = tmp;
+        //If either of the last 2 pots have plants
+        if(newState[workingStateCount - 3] == '#' ||
+           newState[workingStateCount - 4] == '#')
+        {
+            //Grow working state count
+            workingStateCount += 2;
+            
+            //Realloc and initialize new state pots
+            newState = realloc(newState, sizeof(char) * workingStateCount);
+            memset(newState + workingStateCount - 2, '.', sizeof(char) * 2);
+            
+            //Realloc current state pots
+            currentState = realloc(currentState, sizeof(char) * workingStateCount);
+        }
         
-        //Update last state & delta
-        lastStateSum = currentStateSum;
+        //Swap current for new and reset new
+        char *swap = currentState;
+        currentState = newState;
+        newState = swap;
+        memset(newState, '.', sizeof(char) * workingStateCount);
+        
+        //Update last state sum & delta
+        lastStateSum = newStateSum;
         lastDelta = d;
     }
     
@@ -99,16 +138,11 @@ int main(int argc, char *argv[])
     
     //Parse initial state
     FILE *file = fopen(argv[1], "r");
-    char state[256];
-    fscanf(file, "initial state: %s\n\n", state);
-
-    //Initialize initial state with padding
-    size_t stateCount = strlen(state);
-    size_t initialStateCount = stateCount + STATE_PADDING;
-    int zeroIndex = STATE_PADDING / 2;
-    char *initialState = malloc(sizeof(char) * initialStateCount);
-    memset(initialState, '.', sizeof(char) * initialStateCount);
-    memcpy(initialState + zeroIndex, state, stateCount);
+    char initialState[256];
+    fscanf(file, "initial state: %s\n\n", initialState);
+    
+    //Get initial state count
+    int initialStateCount = (int)strlen(initialState);
     
     //Initialize rule result map
     char ruleResultMap[32];
@@ -119,16 +153,18 @@ int main(int argc, char *argv[])
     char ruleResult;
     while(fscanf(file, "%s => %c", rule, &ruleResult) == 2)
     {
-        int ruleIndex = calculateRuleIndex(rule);
-        ruleResultMap[ruleIndex] = ruleResult;
+        if(ruleResult == '#')
+        {
+            int ruleIndex = calculateRuleIndex(rule);
+            ruleResultMap[ruleIndex] = ruleResult;
+        }
     }
     fclose(file);
-
+    
     //Find state sum after twenty generations
     int64_t lastStateSumTwenty = calculateStateSum(ruleResultMap,
                                                    initialState,
                                                    initialStateCount,
-                                                   zeroIndex,
                                                    20);
     printf("part1: state sum after twenty = %lld\n", lastStateSumTwenty);
     
@@ -136,7 +172,6 @@ int main(int argc, char *argv[])
     int64_t lastStateSumFiftyBillion = calculateStateSum(ruleResultMap,
                                                          initialState,
                                                          initialStateCount,
-                                                         zeroIndex,
                                                          50000000000);
     printf("part2: state sum after fifty billion = %lld\n", lastStateSumFiftyBillion);
     
