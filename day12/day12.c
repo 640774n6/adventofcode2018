50b748fad79108bdeb3a34d4c7df6e5a1058a471
Brandon Holland <brandon.holland@catalystrms.com>
2018-12-12 01:55:49 -0800

    Day 12 cleanup pass. TODO: Make more flexible by removing padding and adjusting view until delta has stabalized

--- day12/day12.c
+++ day12/day12.c
@@ -14,6 +14,8 @@
 #define MAX(x, y) (((x) > (y)) ? (x) : (y))
 #define MIN(x, y) (((x) < (y)) ? (x) : (y))
 
+#define STATE_PADDING   300
+
 unsigned int calculateRuleIndex(char *rule)
 {
     unsigned int ruleIndex = 0;
@@ -32,41 +34,57 @@ int64_t calculateStateSum(char *ruleResultMap,
                           int zeroIndex,
                           int64_t generationCount)
 {
+    //Initialize states
     char *newState = malloc(sizeof(char) * stateCount);
     char *currentState = malloc(sizeof(char) * stateCount);
     memcpy(currentState, initialState, sizeof(char) * stateCount);
     
+    //Start generation loop
     int64_t lastStateSum = 0;
     int64_t lastDelta = 0;
     for(int64_t g = 1; g < generationCount + 1; g++)
     {
+        //Get next generation
         int64_t currentStateSum = 0;
         for(int i = 2; i < stateCount - 2; i++)
         {
-            char *check = currentState + i - 2;
-            int ruleIndex = calculateRuleIndex(check);
+            //Get rule for current index
+            //Calculate rule index using rule
+            //Get rule result from map using rule index
+            char *rule = currentState + i - 2;
+            int ruleIndex = calculateRuleIndex(rule);
             char ruleResult = ruleResultMap[ruleIndex];
+            
+            //Set result to index in new state and increment current state sum
             newState[i] = ruleResult;
             currentStateSum += (ruleResult == '#') ? (i - zeroIndex) : 0;
         }
         
+        //If last state sum delta is the same, we have stabalized
         int64_t d = currentStateSum - lastStateSum;
         if(lastDelta == d)
         {
+            //Use stabalized delta to calculate final state sum
             int64_t gDelta = generationCount - g;
             lastStateSum = currentStateSum + (d * gDelta);
             break;
         }
         
+        //Swap current and new
         char *tmp = currentState;
         currentState = newState;
         newState = tmp;
+        
+        //Update last state & delta
         lastStateSum = currentStateSum;
         lastDelta = d;
     }
     
+    //Cleanup
     free(currentState);
     free(newState);
+    
+    //Return last state sum
     return lastStateSum;
 }
 
@@ -84,10 +102,10 @@ int main(int argc, char *argv[])
     char state[256];
     fscanf(file, "initial state: %s\n\n", state);
 
-    //Initialize initial state
+    //Initialize initial state with padding
     size_t stateCount = strlen(state);
-    size_t initialStateCount = stateCount + 5000;
-    int zeroIndex = 4;
+    size_t initialStateCount = stateCount + STATE_PADDING;
+    int zeroIndex = STATE_PADDING / 2;
     char *initialState = malloc(sizeof(char) * initialStateCount);
     memset(initialState, '.', sizeof(char) * initialStateCount);
     memcpy(initialState + zeroIndex, state, stateCount);
